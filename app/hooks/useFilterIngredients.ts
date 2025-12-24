67dee395d9b6b496fe411f7eb03d1768ec988152
toriiy <vikaizhak54@gmail.com>
2025-12-25 01:05:58 +0200

    use ingredients from DB, update opportunity to select ingredient

---
+++ app/hooks/useFilterIngredients.ts
@@ -0,0 +1,35 @@
+import { Ingredient } from "@/lib/generated/prisma/client";
+import { Api } from "@/services/api-client";
+import { useEffect, useState } from "react";
+import { useSet } from "react-use";
+
+interface ReturnProps {
+  ingredients: Ingredient[];
+  loading: boolean;
+  selectedIds: Set<string>;
+  onAddId: (id: string) => void;
+}
+
+export const useFilterIngredients = (): ReturnProps => {
+  const [ingredients, setIngredients] = useState<Ingredient[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  const [selectedIds, { toggle }] = useSet(new Set<string>([]));
+
+  useEffect(() => {
+    async function getIngredients() {
+      try {
+        setLoading(true);
+        const ingredients = await Api.ingredients.getAllIngredients();
+        setIngredients(ingredients);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    getIngredients();
+  }, []);
+
+  return { ingredients, loading, onAddId: toggle, selectedIds };
+};
