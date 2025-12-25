5cb89ce842604dd65d0c645f164bbb7bdcf41935
toriiy <vikaizhak54@gmail.com>
2025-12-26 00:11:53 +0200

    made Filtering items with through query parameters at SideBar

---
+++ app/hooks/useQueryFilters.ts
@@ -0,0 +1,31 @@
+import { useEffect, useRef } from "react";
+import { Filters } from "./useFilters";
+import qs from "qs";
+import { useRouter } from "next/navigation";
+
+export const useQueryFilters = (filters: Filters) => {
+  const isMounted = useRef(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!isMounted.current) {
+      isMounted.current = true;
+      return;
+    }
+
+    const params = {
+      ...filters.prices,
+      pizzaTypes: Array.from(filters.pizzaTypes),
+      sizes: Array.from(filters.sizes),
+      ingredients: Array.from(filters.selectedIngredients),
+    };
+
+    const query = qs.stringify(params, {
+      arrayFormat: "comma",
+    });
+
+    router.push(`?${query}`, {
+      scroll: false,
+    });
+  }, [filters.prices, filters.pizzaTypes, filters.sizes, filters.selectedIngredients, router]);
+};
