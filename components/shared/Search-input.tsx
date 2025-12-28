84a64854f93c37adf17cd7767ce204ad5c31dfa6
toriiy <vikaizhak54@gmail.com>
2025-12-28 11:34:23 +0200

    ProductPage, GroupRoutes, Fixed Next Warmings

--- components/shared/Search-input.tsx
+++ components/shared/Search-input.tsx
@@ -5,6 +5,7 @@ import { Api } from "../../services/api-client";
 import { Product } from "@/lib/generated/prisma/client";
 import { Search } from "lucide-react";
 import Link from "next/link";
+import Image from "next/image";
 import { useRef, useState } from "react";
 import { useClickAway, useDebounce } from "react-use";
 
@@ -73,7 +74,13 @@ export function SearchInput({ className }: Props) {
                 className="flex items-center gap-3 w-full px-3 py-2 hover:bg-primary/10"
                 href={`/product/${product.id}`}
               >
-                <img className="rounded-sm h-8 w-8" src={product.imageUrl} alt={product.name} />
+                <Image
+                  className="rounded-sm"
+                  src={product.imageUrl}
+                  alt={product.name}
+                  width={32}
+                  height={32}
+                />
                 <span>{product.name}</span>
               </Link>
             ))}
